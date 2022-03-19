6fa49836c1bd21a34a24640841925f3742edcd58
keshavgarg987 <83020236+keshavgarg987@users.noreply.github.com>
2022-03-19 17:22:42 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int a, b;
+    printf("enter the number a:");
+    scanf("%d", &a);
+    printf("enter the number b:");
+    scanf("%d", &b);
+    printf("the sum of %d", a + b);
+    getch();
+}
